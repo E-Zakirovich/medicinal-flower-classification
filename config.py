d010cb85cc493e0b7ddfbb2a02e5bad86b12590c
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-07-25 02:31:51 +0500

    dataset transformation codes written for train, validation & test in dataset.py file

---
+++ config.py
@@ -0,0 +1,4 @@
+# image settings
+image_size = 128
+flip_amount = 0.3
+rotation_amount = 10
--- dataset.py
+++ dataset.py
@@ -6,4 +6,43 @@ import config
 
 class Data:
     def __init__(self):
-        ...
+
+        self.transform_dataset = transforms.Compose([
+
+            # make the size of the images same for cnn & nn
+            transforms.Resize(
+                (config.image_size, config.image_size)
+                ),
+
+            # flip & rotate images in order to avoid underfitting & overfitting
+            transforms.RandomHorizontalFlip(p = config.flip_amount),
+            transforms.RandomVerticalFlip(p = config.flip_amount),
+            transforms.RandomRotation(degrees = config.rotation_amount),
+
+            # make a tensor for cnn
+            transforms.ToTensor(),
+
+            # normalization
+            transforms.Normalize(
+                mean = (0.485, 0.456, 0.406),
+                std = (0.229, 0.224, 0.225)
+                )
+            ]
+        )
+
+        self.validation_and_test_transformation = transforms.Compose([
+
+            # make the size of the images same for cnn & nn
+            transforms.Resize(
+                (config.image_size, config.image_size)
+            ),
+
+            # make a tensor for cnn
+            transforms.ToTensor(),
+
+            # normalization
+            transforms.Normalize(
+                mean = (0.485, 0.456, 0.406),
+                std = (0.229, 0.224, 0.225)
+            )
+        ])
