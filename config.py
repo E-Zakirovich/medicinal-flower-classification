f860efe297c770a47b928521d588320780d7695f
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-07-25 02:54:31 +0500

    so many settings created and used inside of dataset.py

--- config.py
+++ config.py
@@ -2,3 +2,13 @@
 image_size = 128
 flip_amount = 0.3
 rotation_amount = 10
+seed = 42
+train_split_amount = 0.8
+val_split_amount = 0.1
+test_split_amount = 0.1
+
+# dataset
+path = "Data/"
+
+# train settings
+batch_size = 32
