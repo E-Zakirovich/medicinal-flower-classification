515b728d0c6e5b670bcf714e40037a6ccd40fe35
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-07-25 02:54:53 +0500

    data loader is now finished.

--- dataset.py
+++ dataset.py
@@ -46,3 +46,68 @@ class Data:
                 std = (0.229, 0.224, 0.225)
             )
         ])
+
+
+    def __load_dataset(self):
+
+        # train dataset transformation
+        train_transform = datasets.ImageFolder(
+            root = config.path,
+            transform = self.transform_dataset
+        )
+
+        # validation and test transformation
+        validation_and_test_transform = datasets.ImageFolder(
+            root = config.path,
+            transform = self.validation_and_test_transformation
+        )
+
+        # generate controllable random numbers
+        generator = torch.Generator().manual_seed(config.seed)
+
+        train_indices, validation_indices,  test_indices = random_split(
+            train_transform,
+            lengths=[config.train_split_amount, config.val_split_amount, config.test_split_amount],
+            generator=generator
+        )
+
+        train_subset = Subset(
+            train_transform,
+            indices=train_indices.indices
+        )
+
+        validation_subset = Subset(
+            validation_and_test_transform,
+            indices=validation_indices.indices
+        )
+
+        test_subset = Subset(
+            validation_and_test_transform,
+            indices=test_indices.indices
+        )
+
+        train = DataLoader(
+            train_subset,
+            batch_size = config.batch_size,
+            shuffle = True,
+            num_workers = 2
+        )
+
+        validation = DataLoader(
+            validation_subset,
+            batch_size=config.batch_size,
+            shuffle=False,
+            num_workers=2
+        )
+
+        test = DataLoader(
+            test_subset,
+            batch_size=config.batch_size,
+            shuffle=False,
+            num_workers=2
+        )
+
+        return train, validation, test
+
+    def loader(self):
+        return self.__load_dataset()
