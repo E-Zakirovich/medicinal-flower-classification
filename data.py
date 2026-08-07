c86811bd08f4908d1a661b47186fcfdeca770233
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-08-07 13:55:54 +0500

    augmentation for train dataset is finished

--- data.py
+++ data.py
@@ -11,8 +11,42 @@ rations.
 import torch
 from torch.utils.data import Subset, DataLoader, random_split
 from torchvision import datasets, transforms
+import config
 
 
 class Dataset:
     def __init__(self):
-        ...
+
+        """
+        here is the place where augmentation happens. following
+        code is transformation for train dataset
+        """
+
+        self.train_dataset_transform = transforms.Compose([
+            # I am changing the size of the image
+            transforms.Resize(
+                (
+                    config.image_size, # image size
+                    config.image_size  # image size
+                )
+            ),
+
+            # half of the dataset images are flipped horizontally,
+            transforms.RandomHorizontalFlip(p = config.horizontal_flip),
+
+            # half of the dataset images are flipped vertically,
+            transforms.RandomHorizontalFlip(p=config.vertical_flip),
+
+            # all dataset images rotated between -a to a degrees
+            transforms.RandomRotation(config.angle),
+
+            # I will make a tensor from images
+            transforms.ToTensor(),
+
+            # normalization part
+            transforms.Normalize(
+                mean = config.mean, # mean
+                std = config.std, # config
+            )
+        ]
+        )
