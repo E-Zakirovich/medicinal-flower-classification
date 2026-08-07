61a92c51cea900de96f1ca20e6b4462d6caa07b8
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-08-07 13:56:22 +0500

    data pipeline setings are stored

--- config.py
+++ config.py
@@ -1,2 +1,10 @@
 # dataset settings
 path = "data/"
+
+# data pipeline settings
+image_size = 256
+horizontal_flip = 0.5
+vertical_flip = 0.5
+angle = 40
+mean = [0.485, 0.456, 0.406]
+std = [0.229, 0.224, 0.225]
